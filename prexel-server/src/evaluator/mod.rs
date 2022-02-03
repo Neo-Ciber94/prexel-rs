42af88cb6fa1643e233b1127967e6994554ad805
Freddy L <neo-ciber_94@hotmail.com>
2022-02-02 21:22:16 -0400

    Changed EvalResult to std::result::Result alias

--- prexel-server/src/evaluator/mod.rs
+++ prexel-server/src/evaluator/mod.rs
@@ -1,11 +1,20 @@
 use crate::models::{EvalExpression, EvalResult};
+use once_cell::sync::Lazy;
 use prexel::complex;
+use prexel::context::Config;
 use prexel::{context::Context, context::DefaultContext, decimal::Decimal, evaluator::Evaluator};
 use std::str::FromStr;
 use std::string::ToString;
 
+static CONFIG: Lazy<Config> = Lazy::new(|| {
+    Config::default()
+        .with_group_symbol('[', ']')
+        .with_group_symbol('(', ')')
+        .with_implicit_mul(true)
+});
+
 pub fn eval_decimal_expression(expression: EvalExpression) -> EvalResult {
-    let mut context = DefaultContext::new_decimal();
+    let mut context = DefaultContext::new_decimal_with_config(CONFIG.clone());
 
     // Set variables
     if let Some(variables) = &expression.variables {
@@ -14,7 +23,7 @@ pub fn eval_decimal_expression(expression: EvalExpression) -> EvalResult {
                 Ok(value) => {
                     context.set_variable(name, value);
                 }
-                Err(err) => return EvalResult::with_error(format!("{}", err)),
+                Err(err) => return EvalResult::Err(format!("{}", err)),
             }
         }
     }
@@ -24,13 +33,13 @@ pub fn eval_decimal_expression(expression: EvalExpression) -> EvalResult {
     let result = evaluator.eval(&expression.expression);
 
     match result {
-        Ok(result) => EvalResult::new(result.to_string()),
-        Err(error) => EvalResult::with_error(error.to_string()),
+        Ok(result) => EvalResult::Ok(result.to_string()),
+        Err(error) => EvalResult::Err(error.to_string()),
     }
 }
 
 pub fn eval_float_expression(expression: EvalExpression) -> EvalResult {
-    let mut context = DefaultContext::new_checked();
+    let mut context = DefaultContext::with_config_checked(CONFIG.clone());
 
     // Set variables
     if let Some(variables) = &expression.variables {
@@ -39,7 +48,7 @@ pub fn eval_float_expression(expression: EvalExpression) -> EvalResult {
                 Ok(value) => {
                     context.set_variable(name, value);
                 }
-                Err(err) => return EvalResult::with_error(format!("{}", err)),
+                Err(err) => return EvalResult::Err(format!("{}", err)),
             }
         }
     }
@@ -49,13 +58,14 @@ pub fn eval_float_expression(expression: EvalExpression) -> EvalResult {
     let result = evaluator.eval(&expression.expression);
 
     match result {
-        Ok(result) => EvalResult::new(result.to_string()),
-        Err(error) => EvalResult::with_error(error.to_string()),
+        Ok(result) => EvalResult::Ok(result.to_string()),
+        Err(error) => EvalResult::Err(error.to_string()),
     }
 }
 
 pub fn eval_complex_expression(expression: EvalExpression) -> EvalResult {
-    let mut context = DefaultContext::new_complex();
+    let mut context =
+        DefaultContext::new_complex_with_config(CONFIG.clone().with_complex_number(true));
 
     // Set variables
     if let Some(variables) = &expression.variables {
@@ -64,7 +74,7 @@ pub fn eval_complex_expression(expression: EvalExpression) -> EvalResult {
                 Ok(value) => {
                     context.set_variable(name, value);
                 }
-                Err(err) => return EvalResult::with_error(format!("{}", err)),
+                Err(err) => return EvalResult::Err(format!("{}", err)),
             }
         }
     }
@@ -74,7 +84,7 @@ pub fn eval_complex_expression(expression: EvalExpression) -> EvalResult {
     let result = evaluator.eval(&expression.expression);
 
     match result {
-        Ok(result) => EvalResult::new(result.to_string()),
-        Err(error) => EvalResult::with_error(error.to_string()),
+        Ok(result) => EvalResult::Ok(result.to_string()),
+        Err(error) => EvalResult::Err(error.to_string()),
     }
 }
--- prexel-server/src/models/eval_result.rs
+++ prexel-server/src/models/eval_result.rs
@@ -1,26 +1,4 @@
 use serde::{Deserialize, Serialize};
 
 /// Represents the result of an evaluation.
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct EvalResult {
-    result: Option<String>,
-    error: Option<String>,
-}
-
-impl EvalResult {
-    /// Creates a new `EvalResult` with the given result.
-    pub fn new(result: String) -> Self {
-        EvalResult {
-            result: Some(result),
-            error: None,
-        }
-    }
-
-    /// Creates a new `EvalResult` with the given error.
-    pub fn with_error(error: String) -> Self {
-        EvalResult {
-            result: None,
-            error: Some(error),
-        }
-    }
-}
+pub type EvalResult = Result<String, String>;
