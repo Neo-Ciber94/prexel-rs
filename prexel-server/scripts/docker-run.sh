6a1319cde5ec8a8dbfc54ba844fc0711cef50114
Freddy L <neo-ciber_94@hotmail.com>
2022-02-04 18:46:26 -0400

    Updated dockerfile

--- prexel-server/scripts/docker-run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-$port=${1:-5000}
-
-cargo +nightly build -Z unstable-options --out-dir=./target/release --release
-
-docker build -t prexel-server:latest .
-
-docker run -it -dp $port:$port -- prexel-server:latest
